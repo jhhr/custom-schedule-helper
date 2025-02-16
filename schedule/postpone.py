68d1568cce8f4e661b7179ada7fea05686446285
jhhr <jormki@gmail.com>
2025-02-16 19:53:12 +0200

    Fix divide by 0, postponing due cards

--- schedule/postpone.py
+++ schedule/postpone.py
@@ -25,10 +25,11 @@ from ..utils import (
     get_last_review_date,
 )
 
-WARNING_TEXT = "You can postpone more cards if you wish, but it is not recommended.\nKeep in mind that whenever you use Postpone or Advance, you depart from the optimal scheduling.\n"
-INFO_TEXT = (
-    "This feature only affects the cards that have been scheduled by Custom Schedule.\n"
+WARNING_TEXT = (
+    "You can postpone more cards if you wish, but it is not recommended.\nKeep in mind that"
+    " whenever you use Postpone or Advance, you depart from the optimal scheduling.\n"
 )
+INFO_TEXT = "This feature only affects the cards that have been scheduled by Custom Schedule.\n"
 INQUIRE_COUNT_TEXT = "Enter the number of cards to be postponed.\n"
 INQUIRE_IVL_TEXT = "Or enter the interval above which the cards will be postponed.\n"
 
@@ -41,7 +42,10 @@ class PostPoneDialog(QDialog):
         self.main_layout = QVBoxLayout(self)
         self.form_layout = QGridLayout()
         self.main_layout.addLayout(self.form_layout)
-        notification_text = f"{'For this deck' if did else 'For this collection'}, it is relatively safe to postpone up to {safe_cnt} cards.\n"
+        notification_text = (
+            f"{'For this deck' if did else 'For this collection'}, it is relatively safe to"
+            f" postpone up to {safe_cnt} cards.\n"
+        )
 
         cnt_label = QLabel(notification_text + WARNING_TEXT + INFO_TEXT)
         self.form_layout.addWidget(cnt_label, 0, 0, 1, 2)
@@ -74,7 +78,8 @@ class PostPoneDialog(QDialog):
         if RepresentsInt(interval):
             interval = int(interval)
             self.interval_view.setText(
-                f"{len(list(filter(lambda x: x[3] >= interval, self.cards)))} cards with interval greater than {interval}."
+                f"{len(list(filter(lambda x: x[3] >= interval, self.cards)))} cards with interval"
+                f" greater than {interval}."
             )
         else:
             self.interval_view.setText("")
@@ -91,7 +96,10 @@ def get_desired_postpone_def_with_response(safe_cnt, did, cards):
 
 
 def get_desired_postpone_cnt_with_response(safe_cnt, did):
-    notification_text = f"{'For this deck' if did else 'For this collection'}, it is relatively safe to postpone up to {safe_cnt} cards.\n"
+    notification_text = (
+        f"{'For this deck' if did else 'For this collection'}, it is relatively safe to postpone up"
+        f" to {safe_cnt} cards.\n"
+    )
     (s, r) = getText(
         INQUIRE_COUNT_TEXT + notification_text + WARNING_TEXT + INFO_TEXT, default="10"
     )
@@ -110,8 +118,7 @@ def postpone(did=None, card_ids=None, parent=None):
 
     # json_extract(data, '$.dr')
     # WHERE data != ''
-    cards = mw.col.db.all(
-        f"""
+    cards = mw.col.db.all(f"""
         SELECT 
             id, 
             CASE WHEN odid==0
@@ -123,6 +130,10 @@ def postpone(did=None, card_ids=None, parent=None):
             CASE WHEN odid==0
             THEN {mw.col.sched.today} - (due - ivl)
             ELSE {mw.col.sched.today} - (odue - ivl)
+            END,
+            CASE WHEN odid==0
+            THEN due
+            ELSE odue
             END
         FROM cards
         WHERE queue = {QUEUE_TYPE_REV}
@@ -130,14 +141,14 @@ def postpone(did=None, card_ids=None, parent=None):
         AND json_extract(json_extract(data, '$.cd'), '$.v') != 'p'
         {"AND id IN %s" % cid_list if card_ids is not None else ""}
         {"AND did IN %s" % did_list if did is not None else ""}
-    """
-    )
+    """)
     # x[0]: cid
     # x[1]: did
     # x[2]: factor
     # x[3]: interval
     # x[4]: elapsed days
-    # x[5]: max interval
+    # x[5]: due
+    # x[6]: max interval
     cards = map(
         lambda x: (
             x
@@ -163,13 +174,8 @@ def postpone(did=None, card_ids=None, parent=None):
         else:
             (desired_postpone_cnt, desired_postpone_interval) = res
             print(desired_postpone_cnt, desired_postpone_interval)
-            if (
-                desired_postpone_cnt is not None
-                and desired_postpone_interval is not None
-            ):
-                showWarning(
-                    "Please enter only either the number of cards or the interval."
-                )
+            if desired_postpone_cnt is not None and desired_postpone_interval is not None:
+                showWarning("Please enter only either the number of cards or the interval.")
                 return
             if (desired_postpone_cnt is not None and desired_postpone_cnt <= 0) or (
                 desired_postpone_interval is not None and desired_postpone_interval <= 0
@@ -195,18 +201,23 @@ def postpone(did=None, card_ids=None, parent=None):
     cnt = 0
     ivl_incr = 0
 
-    for cid, _, fct, ivl, elapsed_days, max_ivl in cards:
+    for cid, _, fct, ivl, elapsed_days, due, max_ivl in cards:
         card = mw.col.get_card(cid)
         random.seed(cid + ivl)
         last_review = get_last_review_date(card)
         elapsed_days = mw.col.sched.today - last_review
+        due_days = max(due - mw.col.sched.today, 0)
         # For cards with ivl < 30, postpone by a percentage of the interval
         delay = elapsed_days - ivl
         msg = f"fct={round(fct / 1000, 2)} Elapsed days: {elapsed_days}, IVL: {ivl}"
-        if elapsed_days < 90:
+        if 0 < elapsed_days < 90:
             # Postpone the card between 5% to ~35% depending on the factor
             # the fct is a number between 1300 and 5000, generally ~2500,
             # so we divide by 15000 to get a number between 0.087 and 0.33, generally ~0.17
+            if fct < 1100:
+                # FSRS factor, convert to an approximate ease factor
+                difficulty = (fct - 100) / 1000
+                fct = 3000 - 1700 * difficulty
             base_mult = max(0.05, fct / 15000)
             # Randomly add a multiplier between 0 and 0.50, giving bigger variance with smaller elapsed days
             # and then reduce the randomness as we go past 7 days.
@@ -225,21 +236,23 @@ def postpone(did=None, card_ids=None, parent=None):
                 max_ivl,  # Don't go over the maximum interval
                 max(
                     1,  # Don't set interval to less than 1
-                    math.floor(
-                        elapsed_days * mult
-                    ),  # Postpone by a percentage of elapsed days
-                    elapsed_days,  # Don't lower the due date
+                    math.floor(elapsed_days * mult)
+                    + due_days,  # Postpone by a percentage of elapsed days
+                    elapsed_days + due_days,  # Don't lower the due date
                 ),
             )
             # Set the increment to the maximum of the new interval and the elapsed days we've postponed
             # so far, thus when start postponing in 1 day increments, we'll start from the next day after
             # the last postponed card.
             ivl_incr = max(new_ivl - elapsed_days, ivl_incr)
-            msg += f" Percentage increase, Delay: {delay}, IVL mult: {round(mult, 2)}, Raw new IVL: {round(elapsed_days * mult, 2)}, New IVL: {new_ivl}, IVL incr: {ivl_incr}"
+            msg += (
+                f" Percentage increase, Delay: {delay}, IVL mult: {round(mult, 2)}, Raw new IVL:"
+                f" {round(elapsed_days * mult, 2)}, New IVL: {new_ivl}, IVL incr: {ivl_incr}"
+            )
         else:
             ivl_incr += 1
             # This card is postponed by 1 more day, the next by 2 more days, and so on.
-            new_ivl = min(elapsed_days + ivl_incr, max_ivl)
+            new_ivl = min(elapsed_days + ivl_incr + due_days, max_ivl)
             msg += f" Fixed increment, New IVL: {new_ivl}, IVL incr: {ivl_incr}"
         print(msg)
         card = update_card_due_ivl(card, new_ivl)
